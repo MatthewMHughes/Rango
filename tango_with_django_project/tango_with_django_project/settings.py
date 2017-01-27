a19f148c64834dfb0895d87770a56743315bfc9d
2200046h <2200046h@student.gla.ac.uk>
2017-01-27 17:12:40 +0000

    Chapter 5 Models

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
 
-STATIC_ROOT = STATIC_DIR
 STATICFILES_DIRS = [STATIC_DIR, ],
 
 MEDIA_ROOT = MEDIA_DIR
