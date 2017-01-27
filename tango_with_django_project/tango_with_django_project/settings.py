b10c679cd8c3f85e6f0a440dfe56de5da9d72661
2200046h <2200046h@student.gla.ac.uk>
2017-01-27 18:08:37 +0000

    Fixed Chapter 4 issues

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -120,9 +120,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATICFILES_DIRS = [STATIC_DIR]
+
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [STATIC_DIR, ],
-
 MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
+
